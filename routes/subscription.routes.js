b426db75f471a04f20d09024571546ba712d5712
architasaha21 <archita.saha2106@gmail.com>
2025-12-24 19:49:31 +0530

    added subscription routes

---
+++ routes/subscription.routes.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.post('/', (req, res) => res.json({ title: 'CREATE a new subscription' }));
+
+subscriptionRouter.get('/', (req, res) => res.json({ title: 'GET all subscriptions' }));
+
+subscriptionRouter.get('/:id', (req, res) => res.json({ title: 'GET subscription details' }));
+
+subscriptionRouter.put('/:id', (req, res) => res.json({ title: 'UPDATE subscription' }));
+
+subscriptionRouter.delete('/:id', (req, res) => res.json({ title: 'DELETE subscription' }));
+
+subscriptionRouter.get('/users/:id', (req, res) => res.json({ title: 'GET all user subscriptions' }));
+
+subscriptionRouter.put('/:id/cancel', (req, res) => res.json({ title: 'CANCEL subscription' }));
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => res.json({ title: 'GET upcoming renewals' }));
+
+
+export default subscriptionRouter;
